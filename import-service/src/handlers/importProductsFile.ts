18e9eacd8da36b26bf0d786773300f360a199d44
Andrei Makarevich <makarka007@gmail.com>
2023-06-16 01:00:54 +0400

    feat: check if file exists in s3

--- import-service/src/handlers/importProductsFile.ts
+++ import-service/src/handlers/importProductsFile.ts
@@ -1,5 +1,5 @@
 import { buildResponce } from "../utils";
-import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { S3Client, PutObjectCommand, HeadObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 const {
@@ -9,9 +9,21 @@ const {
 
 const s3Client = new S3Client({ region: IMPORT_SERVICE_AWS_REGION });
 
+const fileNotExists = async (s3Client: { send: (arg0: any) => any; }, bucket: any, key: any) => {
+  try {
+    await s3Client.send(new HeadObjectCommand({ Bucket: bucket, Key: key }));
+    return true;
+  } catch (error: any) {
+    if (error.name === 'NotFound') {
+      return false;
+    } else {
+      throw error;
+    }
+  }
+}
+
 export const handler = async (event: any) => {
   const fileName = event.queryStringParameters?.name;
-  console.log('filename: ',event);
 
   if (!fileName) {
     return buildResponce(400, "The 'name' query parameter is required.");
@@ -22,17 +34,19 @@ export const handler = async (event: any) => {
     Key: `uploaded/${fileName}`,
     ContentType: "text/csv",
   };
+
+  const fileNotExistsInS3 = await fileNotExists(s3Client, S3_BUCKET_NAME, putObjectParams.Key);
+
+  if (!fileNotExistsInS3) {
+    return buildResponce(400, "The file not exists in the S3 bucket.");
+  }
+
   const putObjectCommand = new PutObjectCommand(putObjectParams);
-
-  console.log('putObjectCommand: ',putObjectCommand);
-
 
   try {
     const signedUrl = await getSignedUrl(s3Client, putObjectCommand, {
       expiresIn: 120,
     });
-
-    console.log('signed_url: ',signedUrl);
 
     return buildResponce(200, JSON.stringify({ url: signedUrl }));
   } catch (error: any) {
